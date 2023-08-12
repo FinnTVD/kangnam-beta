ded263015e76470acab7c8452b17c02ca4c160ff
FinnTVD <finnit.th@gmail.com>
2023-08-12 12:04:55 +0700

    update 12/8

--- components/home/FeatureHome.jsx
+++ components/home/FeatureHome.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import BoxCurrency from './BoxCurrency'
 import SocialMedia from './SocialMedia'
 import { useState } from 'react'
+import classes from './FeatureHome.module.css'
 
 export default function FeatureHome() {
     const [isShow, setIsShow] = useState(null)
@@ -13,17 +14,21 @@ export default function FeatureHome() {
 
     return (
         <ul className='fixed right-[3.94vw] bottom-[5.86vw] z-[9999] gap-y-[1.88vw] flex flex-col transition-all duration-500 ease-linear select-none'>
-            <li className='group relative w-[4.5vw] h-[4.5vw] cursor-pointer shadow-feature flex justify-center items-center bg-white rounded-full before:content-normal before:w-[3.64vw] before:h-[20vw] before:bg-transparent hover:before:block before:hidden before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:-translate-x-[45%]'>
+            <li
+                className={`item-social transition-all duration-200 ease-linear group relative w-[4.5vw] h-[4.5vw] cursor-pointer shadow-feature flex justify-center items-center bg-white rounded-full before:content-normal before:w-[10.64vw] before:h-[25vw] before:bg-transparent hover:before:block before:hidden before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:-translate-x-[58%]`}
+            >
                 <Image
                     src='/social.svg'
                     alt='social'
                     width={36}
                     height={36}
-                    className='object-contain w-[2.04vw] h-[2.04vw]'
+                    className='object-contain w-[2.04vw] h-[2.04vw] group-hover:scale-125 transition-all duration-200 ease-linear'
                 />
                 <SocialMedia />
             </li>
-            <li className='relative w-[4.5vw] h-[4.5vw] shadow-feature bg-white rounded-full'>
+            <li
+                className={`item-social transition-all duration-200 ease-linear relative w-[4.5vw] h-[4.5vw] shadow-feature bg-white rounded-full group`}
+            >
                 <div
                     onClick={handleCurrently}
                     className='relative flex items-center justify-center w-full h-full cursor-pointer '
@@ -33,18 +38,20 @@ export default function FeatureHome() {
                         alt='tiente'
                         width={36}
                         height={36}
-                        className='object-contain w-[2.04vw] h-[2.04vw]'
+                        className='object-contain w-[2.04vw] h-[2.04vw] group-hover:scale-125 transition-all duration-200 ease-linear'
                     />
                 </div>
                 <BoxCurrency className={!isShow && 'hidden'} />
             </li>
-            <li className='w-[4.5vw] h-[4.5vw] cursor-pointer shadow-feature flex justify-center items-center bg-white rounded-full'>
+            <li
+                className={`item-social transition-all duration-200 ease-linear w-[4.5vw] h-[4.5vw] cursor-pointer shadow-feature flex justify-center items-center bg-white rounded-full group`}
+            >
                 <Image
                     src='/call.svg'
                     alt='call'
                     width={36}
                     height={36}
-                    className='object-contain w-[2.04vw] h-[2.04vw]'
+                    className='object-contain w-[2.04vw] h-[2.04vw] group-hover:scale-125 transition-all duration-200 ease-linear'
                 />
             </li>
         </ul>
