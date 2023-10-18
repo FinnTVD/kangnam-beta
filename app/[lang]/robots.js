60cd8787db725f88fa4595fb1c7d76eceac59140
FinnTVD <finnit.th@gmail.com>
2023-10-18 17:03:48 +0700

    fix robots

--- app/[lang]/robots.js
+++ app/[lang]/robots.js
@@ -1,10 +1,10 @@
 export default function robots() {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      // disallow: '/private/',
-    },
-    sitemap: `${process.env.DOMAIN}/sitemap.xml`,
-  }
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            // disallow: '/private/',
+        },
+        sitemap: `${process.env.DOMAIN}sitemap.xml`,
+    }
 }
