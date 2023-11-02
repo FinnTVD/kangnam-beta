8c5d51d77b3ed01d309f083874f136efa411d34b
sonnt <sonnt@toprate.io>
2023-11-02 14:45:53 +0700

    feat: add cdn

--- loader.js
+++ loader.js
@@ -1,4 +1,25 @@
+const isProd = process.env.NODE_ENV === 'production'
 export default function myImageLoader({ src, width, quality }) {
-  const url = src.replace('https://kangnam-okhub.s3.ap-southeast-1.amazonaws.com', 'https://d1zo9jo0ujl2il.cloudfront.net')
-  return `${url}?w=${width}&q=${quality || 75}`
+  // Run development
+  if (!isProd) return `${src}?w=${width}&q=${quality || 75}`
+
+  // Run Production
+  // Public file
+  if (!src.includes(process.env.NEXT_PUBLIC_ORIGIN_URL)) {
+    let url = src
+    if (!!src.includes("/_next/static/media/")) {
+      let urls = src.split('.')
+      urls.splice(-2, 1)
+      url = urls.join(".")
+      url = url.replace('/_next/static/media/', "")
+    } else {
+      url = url.replace('/images/', "")
+    }
+
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/images/public/${url}`
+  }
+
+  // Other file
+  const url = src.replace(process.env.NEXT_PUBLIC_ORIGIN_URL, process.env.NEXT_PUBLIC_CDN_URL)
+  return url
 }
