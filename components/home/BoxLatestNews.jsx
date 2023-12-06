b85db91509f5ce05e79dbadc4ba5771a4bde6298
FinnTVD <finnit.th@gmail.com>
2023-12-06 09:49:47 +0700

    update

--- components/home/BoxLatestNews.jsx
+++ components/home/BoxLatestNews.jsx
@@ -3,7 +3,7 @@ import LatestNews from './LatestNews'
 import { postTypeIdAgreement } from '@/utils'
 
 export default async function BoxLatestNews({ t, lang }) {
-    const dataPosts = await getData('/post')
+    const dataPosts = await getData('/post?order=DESC')
     const dataPostNews = dataPosts?.data?.filter((item) => item?.postType?.id !== postTypeIdAgreement).slice(0, 6)
 
     return (
