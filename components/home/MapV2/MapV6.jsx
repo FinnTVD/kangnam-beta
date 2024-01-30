59d9a7071a755aa51f953ce169c373ac46bfd027
FinnTVD <finnit.th@gmail.com>
2024-01-30 18:24:17 +0700

    update

--- components/home/MapV2/MapV6.jsx
+++ components/home/MapV2/MapV6.jsx
@@ -375,7 +375,6 @@ const MapV6 = ({ lang, dataSlug = '', t }) => {
                 }${status ? '&status=' + status : ''}`,
             )
             const data = await res.json() //data marker
-            console.log('🚀 ~ callApi ~ data:', data)
             if (data && typeof mapRef?.getZoom === 'function') {
                 setIsFirst(false)
                 setDataMap(data)
@@ -548,7 +547,6 @@ const MapV6 = ({ lang, dataSlug = '', t }) => {
             }`,
         )
         const data = await res.json() // data popup
-        console.log('🚀 ~ callDataPopup ~ data:', data)
         if (!data) return
 
         // handle create popup follow levelZoom
